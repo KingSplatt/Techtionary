b78c8e6d26124bacbe87cb7c2f723cf3ba2cc089
jacq1813 <l21170454@culiacan.tecnm.mx>
2024-10-30 20:42:28 -0700

    reconocedor

--- src/Camara.js
+++ src/Camara.js
@@ -6,8 +6,9 @@ const Camara = () => {
     const webcamRef = useRef(null);
     const [model, setModel] = useState(null);
     const [facingMode, setFacingMode] = useState("user");
+    const [predictions, setPredictions] = useState([]);
 
-    // aqui cargamos el modelo
+    // Cargar el modelo
     useEffect(() => {
         const loadModel = async () => {
             const modelURL = `${process.env.PUBLIC_URL}/model/model.json`;
@@ -15,26 +16,57 @@ const Camara = () => {
             setModel(loadedModel);
         };
         loadModel();
-
     }, []);
 
-    // capturar la imagen de la cámara
+    // Capturar la imagen de la cámara
     const capture = useCallback(async () => {
         if (webcamRef.current && model) {
+            setPredictions([]);
             const imageSrc = webcamRef.current.getScreenshot();
-
-            console.log(imageSrc);
+            // Crea una imagen HTML a partir de la cadena de base64 para usar con `fromPixels`
+            const img = new Image();
+            img.src = imageSrc;
+            img.onload = () => predict(img);
         }
     }, [webcamRef, model]);
 
-    // Función para alternar entre cámaras
+    // Alternar entre cámaras
     const toggleFacingMode = () => {
         setFacingMode(prevMode => (prevMode === "user" ? "environment" : "user"));
+
+    };
+
+    // Función de predicción
+    const predict = async (img) => {
+        if (model) {
+            const tImg = tf.browser.fromPixels(img)
+                .resizeNearestNeighbor([224, 224])
+                .toFloat()
+                .expandDims();
+            const prediction = await model.predict(tImg).data();
+
+            const clases = ['Auxiliar', 'Cable de audio RCA', 'Cable coaxial', 'Unidad central de procesamiento (CPU)', 'Disipador', 'Cable DisplayPort', 'Cable DVI', 'Ethernet', 'HDMI', 'Lightning', 'Micro USB', 'Unidad de fuente de alimentación (PSU)', 'Puerto USB A', 'RAM', 'SATA', 'Tarjeta de video', 'Tarjeta madre', 'Teclado', 'USB Tipo A', 'USB Tipo B', 'USB Tipo C', 'VGA', 'Mouse'];
+
+
+            console.log('Probabilidades de cada clase:');
+            clases.forEach((clase, i) => {
+                console.log(`${clase}: ${prediction[i]}`);
+            });
+
+            const maxPrediction = Math.max(...prediction);
+            const maxIndex = prediction.indexOf(maxPrediction);
+            const maxClass = clases[maxIndex];
+            setPredictions([{ className: maxClass, probability: maxPrediction }]);
+
+            console.log('Clase con mayor probabilidad:');
+            console.log({ className: maxClass, probability: maxPrediction });
+
+
+        }
     };
 
     return (
         <div>
-            <h1>Techtionary - Reconocimiento de Imagen</h1>
             <Webcam
                 key={facingMode}
                 audio={false}
@@ -42,9 +74,22 @@ const Camara = () => {
                 screenshotFormat="image/jpeg"
                 videoConstraints={{ facingMode }}
                 className="webcam"
+                mirrored={true}
             />
             <button onClick={capture}>Capturar</button>
             <button onClick={toggleFacingMode}>Cambiar Cámara</button>
+
+            <div>
+                {predictions.map((prediction, i) => (
+                    <div key={i}>
+                        <p style={{ color: "#ffffff" }}>{prediction.className}</p>
+                        <p style={{ color: "#ffffff" }}>{(prediction.probability * 100).toFixed(2)}%</p>
+                    </div>
+                ))}
+            </div>
+
+
+
         </div>
     );
 };
