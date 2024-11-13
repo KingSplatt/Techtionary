8f6f0e746e431499a90eb4735fc053c5b5eb496a
Edgar Alán Fernández Covarrubias <edgar1mas2@gmail.com>
2024-11-12 20:38:15 -0700

    Añadida pestaña de componentes
    Que batalla con el scrollbar, espero no haber arruinado nada jaajja

--- src/Log.js
+++ src/Log.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import Camara from "./Camara";
+import TarjetasDispositivos from "./TarjetasDispositivos";
 
 const Log = () => {
+   
+    const [mostrarCamara, setMostrarCamara] = useState(true);
+
+    const activarCamara = () => {
+        setMostrarCamara(true);
+        document.body.style.overflow = 'hidden';
+    };
+    const desactivarCamara = () => {
+        setMostrarCamara(false);
+        document.body.style.overflow = 'visible';
+    };
+
     return (
         <html>
-            <body>
+            <body >
                 <section id="contenedor">
                     <header class="header">
                         <h1>
@@ -14,18 +28,27 @@ const Log = () => {
                     <section id="secc01">
                         <section id="condos">
                             <section>
-                                <input type="button" value="Camara"></input>
+                                <input type="button" onClick={() => activarCamara()} value="Camara"></input>
                             </section>
                             <section>
-                                <input type="button" value="Dispositivos"></input>
-                            </section>
+                                <input type="button" onClick={() => desactivarCamara()} value="Dispositivos"></input>
                         </section>
                     </section>
-                    <section id="secc02">
-                        <article id="contieneCamara">
-                            <Camara />
-                        </article>
-                    </section>
+                </section>
+            
+                {mostrarCamara ? 
+                
+                <section id="secc02">
+                    <article id="contieneCamara">
+                    <Camara /> 
+                    </article>
+                </section>
+
+                : 
+                <article>
+                <TarjetasDispositivos />
+                </article>
+                }
                 </section>
             </body>
         </html>
---
+++ src/TarjetasDispositivos.js
@@ -0,0 +1,49 @@
+import "./css/estilos.css";
+
+const TarjetasDispositivos = () => {
+    return (
+        <div>
+            <div className="tarjetas">
+            <   div class="card">
+                    <div class="contenidoCard">
+                        <h3><b>Dispositivo 1</b></h3>
+                        <p>Descripción del dispositivo 1</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="contenidoCard">
+                        <h3><b>Dispositivo 2</b></h3>
+                        <p>Descripción del dispositivo 2</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="contenidoCard">
+                        <h3><b>Dispositivo 3</b></h3>
+                        <p>Descripción del dispositivo 3</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="contenidoCard">
+                        <h3><b>Dispositivo 4</b></h3>
+                        <p>Descripción del dispositivo 4</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="contenidoCard">
+                        <h3><b>Dispositivo 4</b></h3>
+                        <p>Descripción del dispositivo 4</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="contenidoCard">
+                        <h3><b>Dispositivo 5</b></h3>
+                        <p>Descripción del dispositivo 5</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default TarjetasDispositivos;
