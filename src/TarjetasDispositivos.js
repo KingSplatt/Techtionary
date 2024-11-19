c9ceb1d187eea13d2e7e4c33acb3cb0506756883
Edgar Alán Fernández Covarrubias <edgar1mas2@gmail.com>
2024-11-18 22:00:14 -0700

    Agregados algunos componentes

--- src/TarjetasDispositivos.js
+++ src/TarjetasDispositivos.js
@@ -4,46 +4,105 @@ const TarjetasDispositivos = () => {
     return (
         <div>
             <div className="tarjetas">
-            <   div class="card">
-            <img class="imgCard" src="https://selectsound.com.mx/cdn/shop/products/cable-hdmi-full-hd-4k-15-mts-select-power-929935_1200x1200.jpg?v=1698923335">
-            </img>
+            <div class="card">
+            <img class="imgCard" src="https://http2.mlstatic.com/D_NQ_NP_887914-MLM47837967232_102021-O.webp"></img>
                     <div class="contenidoCard">
-                        <h3><b>Dispositivo 1</b></h3>
-                        <p>Descripción del dispositivo 1</p>
+                        <h3><b>Alimentación de barril</b></h3>
+                        <p>De los tipos más comunes para transmitir corriente continua (DC) desde una fuente de alimentación a un dispositivo electronico</p>
                     </div>
-                </div>
-                <div class="card">
-                    <div class="contenidoCard">
-                        <h3><b>Dispositivo 2</b></h3>
-                        <p>Descripción del dispositivo 2</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="contenidoCard">
-                        <h3><b>Dispositivo 3</b></h3>
-                        <p>Descripción del dispositivo 3</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="contenidoCard">
-                        <h3><b>Dispositivo 4</b></h3>
-                        <p>Descripción del dispositivo 4</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="contenidoCard">
-                        <h3><b>Dispositivo 4</b></h3>
-                        <p>Descripción del dispositivo 4</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="contenidoCard">
-                        <h3><b>Dispositivo 5</b></h3>
-                        <p>Descripción del dispositivo 5</p>
-                    </div>
-                </div>
             </div>
-            
+            <div class="card">
+            <img class="imgCard" src="https://www.steren.com.mx/media/catalog/product/cache/295a12aacdcb0329a521cbf9876b29e7/image/15916bec3/cable-de-alimentacion-interlock-de-2-m.jpg"></img>
+                    <div class="contenidoCard">
+                        <h3><b>Alimentación de doble barril</b></h3>
+                        <p>Los conectores de barril son cilindros huecos que se utilizan comúnmente para transmitir corriente continua (DC) desde una fuente de alimentación, como un adaptador de corriente, hacia un dispositivo electrónico.</p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhDHjmH5RXHFr5X3ZvMj5ZkDtqioCaBsQHOJiOnmhOzMiSIDhMw_f5cSSqZ0I2MwveHe4xrfgDqj79qXmpYMeZWAEWgPoikHAmUXctlbEIUZpvkUMETOcPgRClpORq2i9LHemqPaFQGYsfe/s1600/pilaa.jpg"></img>
+                    <div class="contenidoCard">
+                        <h3><b>Batería</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src="https://m.media-amazon.com/images/I/61eW6g833-L.jpg"></img>
+                    <div class="contenidoCard">
+                        <h3><b>Ethernet</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src="https://m.media-amazon.com/images/I/718BY-GUm0L._AC_UF1000,1000_QL80_.jpg"></img>
+                    <div class="contenidoCard">
+                        <h3><b>HDMI</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src="https://cloudfront-us-east-1.images.arcpublishing.com/eluniversal/ZZJ7HKKTD5BVJPR3CPF2ATFHS4.jpg"></img>
+                    <div class="contenidoCard">
+                        <h3><b>HDMI (puerto)</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src=""></img>
+                    <div class="contenidoCard">
+                        <h3><b>Jack de audio</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src=""></img>
+                    <div class="contenidoCard">
+                        <h3><b>MicroSD</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src=""></img>
+                    <div class="contenidoCard">
+                        <h3><b>Mouse</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src=""></img>
+                    <div class="contenidoCard">
+                        <h3><b>Procesador</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src=""></img>
+                    <div class="contenidoCard">
+                        <h3><b>Teclado</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src=""></img>
+                    <div class="contenidoCard">
+                        <h3><b>USB tipo A</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src=""></img>
+                    <div class="contenidoCard">
+                        <h3><b>USB tipo B</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            <div class="card">
+            <img class="imgCard" src=""></img>
+                    <div class="contenidoCard">
+                        <h3><b>USB micro</b></h3>
+                        <p></p>
+                    </div>
+            </div>
+            </div>
         </div>
     );
 }
